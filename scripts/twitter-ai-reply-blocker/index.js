6230086aa325a11911209c276cb015d7141d0f4c
Matias Juarez <flipeador@outlook.com>
2023-07-26 23:19:25 -0300

    update blacklist + menu commands

--- scripts/twitter-ai-reply-blocker/index.js
+++ scripts/twitter-ai-reply-blocker/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-alert */
 'use strict';
+
+/* global GM_getValue, GM_setValue, GM_registerMenuCommand */
 
 // utils.js
 /* global mutationObserver */
@@ -9,29 +12,73 @@
 // ==UserScript==
 // @name        Twitter AI Reply Blocker
 // @author      Flipeador
-// @version     1.0.1
+// @version     1.0.2
 // @namespace   https://github.com/flipeador/browser-scripts
 // @homepageURL https://github.com/flipeador/browser-scripts/tree/main/scripts/twitter-ai-reply-blocker
 // @match       https://twitter.com/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
 // @run-at      document-idle
 // @noframes
-// @require     https://raw.githubusercontent.com/flipeador/browser-scripts/c556acdb91be801162223f60352d08ec968256a9/scripts/utils.js
+// @require     https://raw.githubusercontent.com/flipeador/browser-scripts/1c2ce7968b79ec8adc1680ca1918c9d686d4d3ad/scripts/utils.js
 // @require     https://raw.githubusercontent.com/flipeador/browser-scripts/c556acdb91be801162223f60352d08ec968256a9/scripts/twitter.js
 // @downloadURL https://raw.githubusercontent.com/flipeador/browser-scripts/main/scripts/twitter-ai-reply-blocker/index.js
 // ==/UserScript==
 
 // Blacklisted accounts.
-const BLACKLIST = /\s+|@(ReplyGPT|RoflGPTBot|LmaoGPT|BurnGPT|TruthGPTBot|EntertainUsNob|ExplainThisGod|RoastHimJim|RoastMasterYoda|MakeItAQuote|DrawThisTweet|McPepes_AI|GrugThis|ExplainthisRick|FrameHimJim|ExplainThisBean|ExplainThisLiu|ExplainThisTumb|BasedReplyBot|LameGPT|MrShelbySays)/gui;
+const BLACKLIST = ['ReplyGPT', 'RoflGPTBot', 'LmaoGPT', 'BurnGPT', 'TruthGPTBot', 'EntertainUsNob', 'ExplainThisGod', 'RoastHimJim', 'RoastMasterYoda', 'MakeItAQuote', 'DrawThisTweet', 'McPepes_AI', 'GrugThis', 'ExplainthisRick', 'FrameHimJim', 'ExplainThisBean', 'ExplainThisLiu', 'ExplainThisTumb', 'BasedReplyBot', 'LameGPT', 'MrShelbySays', 'MarinethisBob'];
 // Accounts with at least this amount of followers will never be blocked.
 const MIN_FOLLOWERS_COUNT = 5000;
 
 const ignoreList = [];
 
+function escapeRegEx(str) {
+    // https://stackoverflow.com/a/3561711/14822191
+    return str.replace(/[/\-\\^$*+?.()|[\]{}]/gu, '\\$&');
+}
+
+function addUserToBlacklist(user) {
+    user = escapeRegEx(user.replace(/\s+|@/gu, ''));
+    if (!user || BLACKLIST.includes(user)) return;
+    const list = GM_getValue('blacklist') ?? [];
+    if (!list.includes(user))
+      GM_setValue('blacklist', [...list, user]);
+}
+
+function removeUserFromBlacklist(user) {
+    const list = GM_getValue('blacklist') ?? [];
+    const index = list.indexOf(user);
+    if (index === -1) return;
+    list.splice(index, 1);
+    GM_setValue('blacklist', list);
+}
+
+GM_registerMenuCommand(
+    'Add user to blacklist',
+    () => addUserToBlacklist(prompt('Username:'))
+);
+
+GM_registerMenuCommand(
+    'Remove user from blacklist',
+    () => removeUserFromBlacklist(prompt('Username:'))
+);
+
+GM_registerMenuCommand(
+    'Show blacklist',
+    () => alert(`${BLACKLIST.join('|')}\n\n${(GM_getValue('blacklist')??[]).join('|')}`)
+);
+
 function checkTweetText(text) {
-    return (
-        text &&
-        text.replaceAll(BLACKLIST, '') === ''
-    );
+    if (!text) return;
+
+    const blacklist = [
+        ...BLACKLIST,
+        ...GM_getValue('blacklist') ?? []
+    ].join('|');
+
+    const re = new RegExp(`\\s+|@(${blacklist})`, 'gui');
+    return text.replace(re, '') === '';
 }
 
 mutationObserver(async () => {
