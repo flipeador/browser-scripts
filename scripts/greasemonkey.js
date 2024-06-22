2643ded64b4213b6a0a34721e61122f9ada25490
Matías Juarez <flipeador@outlook.com>
2024-06-22 20:54:02 -0300

    update greasemonkey.js

--- scripts/greasemonkey.js
+++ scripts/greasemonkey.js
@@ -1,13 +1,26 @@
-/* eslint-disable no-unused-vars */
 'use strict';
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_registerMenuCommand
 
-function registerToggleMenuCmd(key, name) {
-    const action = GM_getValue(key) ? 'Disable' : 'Enable';
-    registerToggleMenuCmd[key] = GM_registerMenuCommand(
-        `${action}: ${name}`, () => {
-            GM_setValue(key, !GM_getValue(key));
-            GM_unregisterMenuCommand(registerToggleMenuCmd[key]);
-            registerToggleMenuCmd(key, name);
-        }
+// eslint-disable-next-line no-unused-vars
+function regToggleMenuCmd(caption, callback, key, id) {
+    let key2 = typeof(key) === 'string';
+    if (key2) key = GM_getValue(key2 = key);
+    const text = `${key?'✅':'❌'} ${caption}`;
+
+    const obj = { };
+    obj.id = GM_registerMenuCommand(
+        text,
+        async (event) => {
+            event.enabled = !key;
+            const value = await callback?.(event);
+            if (value === null) return;
+            if (typeof(value) === 'string')
+                caption = value;
+            if (key2) GM_setValue(key2, !key);
+            regToggleMenuCmd(caption, callback, key2||!key, obj.id);
+        },
+        { id: id ?? text, autoClose: false }
     );
 }
